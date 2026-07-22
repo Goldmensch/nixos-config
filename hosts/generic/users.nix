ce0f63b3e5ed11acc09031788d97e5e7be002514
Goldmensch <pm@goldmensch.dev>
2026-07-22 21:13:56 +0200

    Add password hash

--- hosts/generic/users.nix
+++ hosts/generic/users.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }: {
+  users.mutableUsers = false;
   users.users.nick = {
     isNormalUser = true;
     description = "Nick Hensel";
     extraGroups = [ "networkmanager" "wheel" "seat"];
     shell = pkgs.zsh;
-    initialPassword = "test";
+    hashedPassword = "$y$j9T$ATjsOx5in3cz8Xt723eGx/$5lQEnDc4wDFzoVZNezwk7e6WEKLqvzdZvswT/tq/Pc.";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqW5HZq5oU8xBstcyA2JzL2iGZ69Cj6jyAL14p/uPs/ openpgp:0xFBFB79F7"
     ];
